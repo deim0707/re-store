1cdbf7d804774e8c2f3f06bcd45ab22fba9195ce
deim0707 <dima1parshin@gmail.com>
2020-04-17 22:33:45 +0300

    Создал начальную структуру проекта.

---
+++ src/components/hoc/with-bookstore-service.js
@@ -0,0 +1,21 @@
+//используем этот HOC для большего удобства работы с контекстом (в котором передаём Сервис в другие компоненты)
+import React from 'react';
+import { BookstoreServiceConsumer } from '../bookstore-service-context';
+
+const withBookstoreService = () => (Wrapped) => {
+
+  return (props) => {
+    return (
+      <BookstoreServiceConsumer>
+        {
+          (bookstoreService) => {
+            return (<Wrapped {...props}
+                     bookstoreService={bookstoreService}/>);
+          }
+        }
+      </BookstoreServiceConsumer>
+    );
+  }
+};
+
+export default withBookstoreService;
