939a2ab2b0d1c2ea0abe5495f11130acd5698dfa
deim0707 <dima1parshin@gmail.com>
2020-04-26 01:44:39 +0300

    Корзина учитывает, что данный товар уже имеется и меняет только счётчик товаров и цену

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,6 +7,54 @@ const initialState = {
     orderTotal: 225
 };
 
+//функции для чистоты кода в редюсере
+const updateCartItems = (cartItems, item, idx) => {
+    if (idx === -1) { //если индекс -1, то есть у нас новый элемент. то возвращаем новый массив. который состоит из всех элемент cartItems и новый эллемент
+        return [...cartItems, item]
+    }
+    //если индекс любое другое число. то обновляем существующий
+    return [
+        ...cartItems.slice(0, idx),//от начала массива до выбранного индекса он буде как раньше
+        item, //вставим наш новый элемент
+        ...cartItems.slice(idx +1) //после вставляем старую часть. после нового индекса
+    ]
+};
+
+const updateCartItem = (book, item = {}) => {
+    //тут либо значения из существоющего айтема, либо первого выбранного*
+    const {
+        id = book.id,
+        count=0,
+        title=book.title,
+        total=0} = item;
+
+    return {
+        id,
+        title,
+        count: count+1,
+        total: total + book.price
+    }
+
+    //ИННОЕ НАПИСАНИЕ ЭТОЙ ФУНКЦИИ
+    //если у нас есть старый айтем (если айтем не равен undefined, т.к. при индексе -1 будет undefined), то, создаём новый на основании старого
+    // if (item) {
+    //     //тут мы берём все из уже существующей книги, и меняем только пару полей
+    //     return  {
+    //         ...item,
+    //         count: item.count+1,
+    //         total: item.total + book.price
+    //     };
+    // }
+    // else {             //если нет, то создаём новый
+    //     return  {
+    //         id: book.id,
+    //         title: book.title,
+    //         count: 1,
+    //         total: book.price
+    //     };
+    // }
+};
+
 const reducer = (state = initialState, action) => {
     // console.log(action.type); //какой экшен поступил в редюсер
     switch (action.type) {
@@ -39,18 +87,14 @@ const reducer = (state = initialState, action) => {
         case 'BOOK_ADDED_TO_CART': {
             const bookId = action.payload;
             const book = state.books.find((book) => book.id===bookId);
-            const newBook = {
-                id: book.id,
-                name: book.title,
-                count: 1,
-                total: book.price
-            };
+            //проверка на наличие в корзине элемента с которым работаем
+            const itemIndex = state.cartItems.findIndex(({id})=> id === bookId); //айди элемента, если он уже есть в списке (если нет, то вернёт -1)
+            const item = state.cartItems[itemIndex];
+            const newItem = updateCartItem(book,item)
+
             return {
                 ...state,
-                cartItems: [
-                    ...state.cartItems,
-                    newBook
-                ]
+                cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
             }
         }
 
